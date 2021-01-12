d04e60a7576d9e42ee2709e786a93709927ed68b
João Santos <j_ssfilho@hotmail.com>
2021-01-11 23:58:40 -0300

    Iniciando projeto clone netflix com firebase

---
+++ NetflixClone/app/src/main/java/br/com/ifpb/lampiao/netflixclone/FormCadastro.kt
@@ -0,0 +1,11 @@
+package br.com.ifpb.lampiao.netflixclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_cadastro)
+    }
+}
---
+++ NetflixClone/app/src/main/java/br/com/ifpb/lampiao/netflixclone/FormLogin.kt
@@ -0,0 +1,22 @@
+package br.com.ifpb.lampiao.netflixclone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class FormLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_login)
+
+        var cadastro = findViewById<TextView>(R.id.text_cadastrar)
+        cadastro.setOnClickListener {
+            AbrirTelaDeCadastro()
+        }
+    }
+    private fun AbrirTelaDeCadastro(){
+        var intent = Intent(this,FormCadastro::class.java)
+        startActivity(intent)
+    }
+}
