57401fd085322ba6e67fd3c72ed1248d999da179
Joao Santos <j_ssfilho@hotmail.com>
2021-01-15 22:10:58 -0300

    criação e login de usuario criados

--- LojaVirtual/app/src/main/java/br/edu/ifpb/lampiao/lojavirtual/Form/FormCadastro.kt
+++ LojaVirtual/app/src/main/java/br/edu/ifpb/lampiao/lojavirtual/Form/FormCadastro.kt
@@ -1,22 +1,71 @@
 package br.edu.ifpb.lampiao.lojavirtual.Form
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import br.edu.ifpb.lampiao.lojavirtual.R
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class FormCadastro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_cadastro)
         supportActionBar!!.hide()
+
+        var cadastrar = findViewById<TextView>(R.id.bt_cadastro)
+        cadastrar.setOnClickListener {
+            CadastrarUsuario()
+        }
+
+    }
+
+    private fun CadastrarUsuario(){
         var edit_email = findViewById<EditText>(R.id.email_cadastro).text.toString()
         var edit_senha = findViewById<EditText>(R.id.senha_cadastro).text.toString()
-        var cadastrar = findViewById<TextView>(R.id.text_cadastro)
-        cadastrar.setOnClickListener {
-           
+
+        if (edit_email.isEmpty() || edit_senha.isEmpty()){
+
+            var snackbar = Snackbar.make(findViewById(R.id.layout_cadastro),"Coloque seu email e sua senha",Snackbar.LENGTH_INDEFINITE)
+                    .setBackgroundTint(Color.WHITE)
+                    .setTextColor(Color.BLACK).setAction("OK", View.OnClickListener {
+
+                    })
+            snackbar.show()
+        }else{
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(edit_email,edit_senha).addOnCompleteListener {
+                if (it.isSuccessful){
+
+                    var snackbar = Snackbar.make(findViewById(R.id.layout_cadastro),"Cadastro Realizado com Sucesso!",Snackbar.LENGTH_INDEFINITE)
+                            .setBackgroundTint(Color.WHITE)
+                            .setTextColor(Color.BLACK).setAction("OK", View.OnClickListener {
+                                voltarParaLogin()
+                            })
+                    snackbar.show()
+                }
+            }.addOnFailureListener {
+
+
+                var snackbar = Snackbar.make(findViewById(R.id.layout_cadastro),"Erro ao cadastrar usuario",Snackbar.LENGTH_INDEFINITE)
+                        .setBackgroundTint(Color.WHITE)
+                        .setTextColor(Color.BLACK).setAction("OK", View.OnClickListener {
+
+                        })
+                snackbar.show()
+            }
         }
     }
+
+    private fun voltarParaLogin() {
+        var intent = Intent(this,FormLogin::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+
 }
--- LojaVirtual/app/src/main/java/br/edu/ifpb/lampiao/lojavirtual/Form/FormLogin.kt
+++ LojaVirtual/app/src/main/java/br/edu/ifpb/lampiao/lojavirtual/Form/FormLogin.kt
@@ -1,23 +1,70 @@
 package br.edu.ifpb.lampiao.lojavirtual.Form
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.TextView
+import android.os.Handler
+import android.view.View
+import android.widget.*
 import br.edu.ifpb.lampiao.lojavirtual.R
+import br.edu.ifpb.lampiao.lojavirtual.TelaPrincipal
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class FormLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_login)
         supportActionBar!!.hide()
-        var edit_email = findViewById<EditText>(R.id.email_login).text.toString()
-        var edit_senha = findViewById<EditText>(R.id.senha_login).text.toString()
+
         var tela_cadastro = findViewById<TextView>(R.id.text_cadastro)
+        var login_bt = findViewById<Button>(R.id.bt_login)
         tela_cadastro.setOnClickListener {
             var intent = Intent(this,FormCadastro::class.java)
             startActivity(intent)
         }
+        login_bt.setOnClickListener {
+            login()
+        }
+
+    }
+    private fun login(){
+        var edit_email = findViewById<EditText>(R.id.email_login).text.toString()
+        var edit_senha = findViewById<EditText>(R.id.senha_login).text.toString()
+        if (edit_email.isEmpty() || edit_senha.isEmpty()){
+
+            var snackbar = Snackbar.make(findViewById(R.id.layout_login),"Coloque seu email e sua senha",
+                Snackbar.LENGTH_INDEFINITE)
+                .setBackgroundTint(Color.WHITE)
+                .setTextColor(Color.BLACK).setAction("OK", View.OnClickListener {
+
+                })
+            snackbar.show()
+        }else{
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(edit_email,edit_senha).addOnCompleteListener {
+                if (it.isSuccessful){
+                    findViewById<FrameLayout>(R.id.frameL).visibility = View.VISIBLE
+                    Handler().postDelayed({abrirTelaPrincipal()},3000)
+                    
+
+                }
+            }.addOnFailureListener {
+
+
+                var snackbar = Snackbar.make(findViewById(R.id.layout_login),"Erro ao logar usuario",
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setBackgroundTint(Color.WHITE)
+                    .setTextColor(Color.BLACK).setAction("OK", View.OnClickListener {
+
+                    })
+                snackbar.show()
+            }
+        }
+    }
+    private fun abrirTelaPrincipal(){
+        var intent =Intent(this,TelaPrincipal::class.java)
+        startActivity(intent)
+        finish()
     }
 }
